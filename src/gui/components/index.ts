811211e76b956ea170371e81708f28122862fb3d
bartytime4life <blackbart555@gmail.com>
2025-08-31 13:10:26 -0500

    Create barrel file for GUI components

---
+++ src/gui/components/index.ts
@@ -0,0 +1,23 @@
+// src/gui/components/index.ts
+// ============================================================================
+// 📦 Barrel file for GUI components
+// ----------------------------------------------------------------------------
+// This file re-exports all components so that imports elsewhere in the GUI
+// can be simplified. For example:
+//
+//   import { Button, Card, Table } from "@/gui/components";
+//
+// Instead of importing each component individually from its path.
+// ----------------------------------------------------------------------------
+
+export { default as Card } from "./Card";
+export { default as Chart } from "./Chart";
+export { default as Table } from "./Table";
+export { default as Panel } from "./Panel";
+export { default as Button } from "./Button";
+export { default as Input } from "./Input";
+export { default as Select } from "./Select";
+export { default as Tabs } from "./Tabs";
+export { default as Modal } from "./Modal";
+export { default as Loader } from "./Loader";
+export { default as Tooltip } from "./Tooltip";
